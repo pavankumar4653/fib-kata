b08c0033eac9b32861bf6506450da8efa38e4dea
pavansai003 <pavansai15@iiitkottayam.ac.in>
2020-11-07 18:35:40 +0530

    changes to pavan

--- main.js
+++ main.js
@@ -1,28 +1,5 @@
 function main(num) {
-
-  function fibonacci_series(n) {
-    if (n === 1) {
-      return [0, 1];
-    }
-    else {
-      var s = fibonacci_series(n - 1);
-      s.push(s[s.length - 1] + s[s.length - 2]);
-      return s;
-    }
-
-
-  };
-
-
-  var fibarray = fibonacci_series(100);
-  if (num < 0) {
-    console.log("Number cant be negative");
-  }
-  else {
-    return fibarray[num]; 
-  }
-
-
+  return kumar
 }
-main(5);
+
 module.exports = main
